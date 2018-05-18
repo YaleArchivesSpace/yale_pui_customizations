3628c80ec23555409398e7e97c2a32be782fd740
Mark Custer <fordmadox@gmail.com>
2018-05-17 22:04:03 -0400

    gotta figure out javascript overrides another way; moved csv exports into another plugin

--- public/plugin_init.rb
+++
@@ -1,8 +0,0 @@
-# read in routes
-my_routes = File.join(File.dirname(__FILE__), "routes.rb")
-Plugins.extend_aspace_routes(my_routes)
-
-AppConfig[:pui_page_custom_actions] << {
-   'record_type' => ['resource'],
-   'erb_partial' => 'shared/csv'
-   }
--- public/routes.rb
+++
@@ -1,3 +0,0 @@
-ArchivesSpacePublic::Application.routes.draw do
-  match 'repositories/:rid/resources/:id/csv' => 'resources_addons#csv_out', :via => [:get], defaults: { format: 'csv' }  
-end
